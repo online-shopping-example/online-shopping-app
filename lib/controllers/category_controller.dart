09df6b251a6bd103631daa87dca5271db0149c83
Ammar400Shannar <Ammar.shannar400@gmail.com>
2025-04-14 23:33:11 +0300

    Build Some Widget
    Signed-off-by: Ammar400Shannar <Ammar.shannar400@gmail.com>

---
+++ lib/controllers/category_controller.dart
@@ -0,0 +1,93 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:online_shopping_app/database/dp_category.dart';
+import 'package:online_shopping_app/models/category_model.dart';
+
+class CategoryController {
+  // getting specific category.
+  static Future<CategoriesModel?> getCategory(String categoryId) async {
+    try {
+      DocumentSnapshot categoryDoc = await DBCategory.getDBCategory(categoryId);
+      if (categoryDoc.exists) {
+        CategoriesModel categoriesModel = CategoriesModel.fromJson(
+            categoryDoc.data() as Map<String, dynamic>, categoryId);
+        return categoriesModel;
+      }
+      return null;
+    } catch (e) {
+      debugPrint('There was an error with getting the category $e');
+      rethrow;
+    }
+  }
+
+  // adding a new category.
+  static Future<CategoriesModel?> addCategory(
+      CategoriesModel categoriesModel) async {
+    try {
+      String categoryID =
+          await DBCategory.addDBCategory(categoriesModel.toJson());
+      return categoriesModel.copy(id: categoryID);
+    } catch (e) {
+      debugPrint('There was an error with adding the category $e');
+      rethrow;
+    }
+  }
+
+// updating a category.
+  static Future<void> updateCategory(CategoriesModel categoriesModel) async {
+    try {
+      await DBCategory.updateDBCategory(
+          categoriesModel.id!, categoriesModel.toJson());
+    } catch (e) {
+      debugPrint('There was an error with updating the category $e');
+      rethrow;
+    }
+  }
+
+// deleting a category.
+  static Future<void> deleteCategory(CategoriesModel categoriesModel) async {
+    try {
+      await DBCategory.deleteDBCategory(categoriesModel.id!);
+    } catch (e) {
+      debugPrint('There was an error with deleting the category $e');
+      rethrow;
+    }
+  }
+
+// getting all categories as Future.
+  static Future<List<CategoriesModel>> gettingAllCategories() async {
+    try {
+      List<QueryDocumentSnapshot> allCategoriesQuery =
+          await DBCategory.getAllDBCategories();
+      List<CategoriesModel> allCategories = [];
+      for (QueryDocumentSnapshot categoryDoc in allCategoriesQuery) {
+        if (categoryDoc.exists) {
+          CategoriesModel newCategoriesModel = CategoriesModel.fromJson(
+              categoryDoc.data() as Map<String, dynamic>, categoryDoc.id);
+          allCategories.add(newCategoriesModel);
+        }
+      }
+      return allCategories;
+    } catch (e) {
+      debugPrint('There was an error with getting all the category  $e');
+      rethrow;
+    }
+  }
+
+  // getting all categories as Stream
+  static Stream<List<CategoriesModel>> getAllCategoriesAsStream() {
+    Stream<QuerySnapshot> snapShot = DBCategory.getAllDBCategoriesAsStream();
+    return snapShot.map((snapShot) {
+      try {
+        List<CategoriesModel> categories = snapShot.docs.map((doc) {
+          Map<String, dynamic> dataOfDoc = doc.data() as Map<String, dynamic>;
+          return CategoriesModel.fromJson(dataOfDoc, doc.id);
+        }).toList();
+        return categories;
+      } catch (e) {
+        debugPrint('Error in conversion: $e');
+        rethrow;
+      }
+    });
+  }
+}
